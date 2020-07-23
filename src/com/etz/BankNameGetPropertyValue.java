23dc99369d1ff041f9dd0069adccf014a1c5eb37
Jennifer Okosisi <jennifer.okosisi@etranzactng.com>
2020-07-23 11:49:57 +0100

    Bank

---
+++ src/com/etz/BankNameGetPropertyValue.java
@@ -0,0 +1,5 @@
+package com.etz;
+
+public class BankNameGetPropertyValue {
+    
+}
---
+++ src/com/etz/BankNameReadConfigMain.java
@@ -0,0 +1,13 @@
+
+package com.etz;
+
+import java.io.IOException;
+
+public class BankNameReadConfigMain {
+
+    public static voidon main(String[] args) throws IOException;
+    cashTransaction properties = new cashTransaction();
+    properties.cashTransaction;
+           }
+    
+}
